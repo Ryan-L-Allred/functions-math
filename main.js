02042e16dba0fbbc38ce449eae359d94b90279b6
Ryan Allred <ryanallred91@gmail.com>
2022-10-07 10:24:25 -0500

    pushing more up

---
+++ main.js
@@ -0,0 +1,12 @@
+const addTwoNumbers = (add1, add2) => {
+    return add1 + add2
+}
+const add = addTwoNumbers(7, 12)
+
+
+const multiplyNumbers = (factor) => {
+    return factor * 2
+}
+const multiply = multiplyNumbers(add)
+console.log(multiply)
+
